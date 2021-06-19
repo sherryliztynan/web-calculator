4de48b5901cd7cabf8cea6f2fdc2b0ddf5a9b164
Sherron <sherron@Sherrons-MacBook-Air.local>
2021-06-19 11:21:14 -0400

    updated style and javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+function sum() {
+    var numberinput1 = Number(document.getElementById("numberinput1").value);
+    console.log(numberinput1)
+    var numberinput2 = Number(document.getElementById("numberinput2").value);
+    console.log(numberinput2)
+    var result = document.getElementById("result").value=(numberinput1 + numberinput2);
+    console.log(result)
+    }
+
+
+    // var button = document.getElementById("sum")
+    // button.addEventListener("click",function(){
+    // sum()    
+    // })
+     
